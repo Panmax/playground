d103ec9da8f6fc166855681f4208938f34e56b7d
Panmax <967168@qq.com>
2016-01-23 22:15:48 +0800

    api send notify

--- app/music/views.py
+++ app/music/views.py
@@ -10,18 +10,7 @@ from . import music
 
 @music.route('')
 def index():
-    name = request.args.get('name', '')
-    musics = []
-    if name:
-        url = u"http://so.ard.iyyin.com/s/song_with_out?q=%s&page=1&size=10" % name
-        response = requests.get(url)
-        musics = json.loads(response.text).get('data')
-    valid_musics = [music for music in musics if not music.get('out_list')]
-    redis_op.notify('channel:music:download', event={
-        'musics': valid_musics
-    })
-
-    return render_template('music/index.html', musics=valid_musics, name=name)
+    return render_template('music/index.html')
 
 
 @music.route('/api/get_musics')
@@ -33,4 +22,7 @@ def api_get_musics():
         response = requests.get(url)
         musics = json.loads(response.text).get('data')
     valid_musics = [music for music in musics if not music.get('out_list')]
+    redis_op.notify('channel:music:download', event={
+        'musics': valid_musics
+    })
     return make_response(json.dumps(valid_musics))
