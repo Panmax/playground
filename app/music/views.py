f25b1ca4694b4fbe97d11c295cefe1e82578729a
Panmax <967168@qq.com>
2016-01-23 22:05:50 +0800

    异步加载音乐

--- app/music/views.py
+++ app/music/views.py
@@ -2,8 +2,7 @@
 import json
 
 import requests
-from flask import render_template
-from flask import request
+from flask import request, render_template, jsonify, make_response
 
 from utils import redis_op
 from . import music
@@ -23,3 +22,15 @@ def index():
     })
 
     return render_template('music/index.html', musics=valid_musics, name=name)
+
+
+@music.route('/api/get_musics')
+def api_get_musics():
+    keyword = request.args.get('keyword', '')
+    musics = []
+    if keyword:
+        url = u"http://so.ard.iyyin.com/s/song_with_out?q=%s&page=1&size=10" % keyword
+        response = requests.get(url)
+        musics = json.loads(response.text).get('data')
+    valid_musics = [music for music in musics if not music.get('out_list')]
+    return make_response(json.dumps(valid_musics))
--- app/static/js/music.js
+++ app/static/js/music.js
@@ -1,8 +1,10 @@
 /**
  * Created by pan on 16/1/5.
  */
+
 new Vue({
     el: '#app',
+
     data: {
         play_url: '',
         is_playing: false,
@@ -11,8 +13,21 @@ new Vue({
                     audio.load(url);
                     audio.play()
                 }
-            })
+            }),
+        not_like: {
+            'glyphicon':true,
+            'glyphicon-heart-empty': true
+        },
+        keyword: '',
+        musics: null
     },
+
+    created: function () {
+    },
+
+    watch: {
+    },
+
     methods: {
         playMusic: function (url) {
             this.play_url = url;
@@ -24,6 +39,20 @@ new Vue({
         },
         downLoad: function (url) {
             window.open(url)
+        },
+        searchMusic: function () {
+            var xhr = new XMLHttpRequest();
+            var self = this;
+            xhr.open('GET', '/music/api/get_musics?keyword=' + this.keyword);
+            xhr.onload = function () {
+                self.musics = JSON.parse(xhr.responseText)
+            };
+            xhr.send();
+        },
+        likeMusic: function (song_id) {
+            this.not_like['glyphicon-heart-empty'] = false;
+            this.not_like['glyphicon-heart'] = true;
+            this.not_like['text-danger'] = true;
         }
     }
 })
